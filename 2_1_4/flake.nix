e9d66d8fab3d96b12ca1445660654a229933d653
hugosenari <hugosenari@gmail.com>
2022-03-12 04:34:52 -0300

    chore: re index norman

---
+++ 2_1_4/flake.nix
@@ -0,0 +1,50 @@
+{
+  description = ''Migration manager for Norm.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-norman-2_1_4.flake = false;
+  inputs.src-norman-2_1_4.owner = "moigagoo";
+  inputs.src-norman-2_1_4.ref   = "refs/tags/2.1.4";
+  inputs.src-norman-2_1_4.repo  = "norman";
+  inputs.src-norman-2_1_4.type  = "github";
+  
+  inputs."norm".dir   = "nimpkgs/n/norm";
+  inputs."norm".owner = "riinr";
+  inputs."norm".ref   = "flake-pinning";
+  inputs."norm".repo  = "flake-nimble";
+  inputs."norm".type  = "github";
+  inputs."norm".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."norm".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."cligen".dir   = "nimpkgs/c/cligen";
+  inputs."cligen".owner = "riinr";
+  inputs."cligen".ref   = "flake-pinning";
+  inputs."cligen".repo  = "flake-nimble";
+  inputs."cligen".type  = "github";
+  inputs."cligen".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."cligen".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."dotenv".dir   = "nimpkgs/d/dotenv";
+  inputs."dotenv".owner = "riinr";
+  inputs."dotenv".ref   = "flake-pinning";
+  inputs."dotenv".repo  = "flake-nimble";
+  inputs."dotenv".type  = "github";
+  inputs."dotenv".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."dotenv".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-norman-2_1_4"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-norman-2_1_4";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = ''Migration manager for Norm.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."norman-develop".url = "path:./develop";
+  inputs."norman-develop".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."norman-develop".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."norman-master".url = "path:./master";
+  inputs."norman-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."norman-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."norman-1_0_0".url = "path:./1_0_0";
+  inputs."norman-1_0_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."norman-1_0_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."norman-1_0_1".url = "path:./1_0_1";
+  inputs."norman-1_0_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."norman-1_0_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."norman-1_0_2".url = "path:./1_0_2";
+  inputs."norman-1_0_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."norman-1_0_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."norman-2_0_0".url = "path:./2_0_0";
+  inputs."norman-2_0_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."norman-2_0_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."norman-2_1_0".url = "path:./2_1_0";
+  inputs."norman-2_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."norman-2_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."norman-2_1_1".url = "path:./2_1_1";
+  inputs."norman-2_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."norman-2_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."norman-2_1_2".url = "path:./2_1_2";
+  inputs."norman-2_1_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."norman-2_1_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."norman-2_1_3".url = "path:./2_1_3";
+  inputs."norman-2_1_3".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."norman-2_1_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."norman-2_1_4".url = "path:./2_1_4";
+  inputs."norman-2_1_4".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."norman-2_1_4".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."norman-2_1_5".url = "path:./2_1_5";
+  inputs."norman-2_1_5".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."norman-2_1_5".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."norman-2_1_7".url = "path:./2_1_7";
+  inputs."norman-2_1_7".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."norman-2_1_7".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
